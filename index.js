75ca3722e57093b13f08dae3e42e85d8a224d39c
C.A <per.aspera.ad.astra.etc@gmail.com>
2023-05-27 22:51:38 -0700

    Fixed bug with doubled currentNum

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let errorText = document.querySelector("#errorText");
 
 let deleteLastButton = document.querySelector('#deleteLast'); 
 deleteLastButton.addEventListener('click', () => {
-    example.eraseLast(); 
+    example.deleteCurrentNum(); 
     runningTotal.textContent = example.getNums(); 
 }) 
 
@@ -18,9 +18,39 @@ numButtons.forEach(numButton => {
     })
 })
 
+document.addEventListener('keydown', (event) => {
+    var name = event.key;
+    var code = event.code;
+
+    if (name == "1" || name == "2" || name == "3" || name == "4" || name == "5" || name == 
+    "6" || name == "7" || name == "8" || name == "9" || name == ".") {
+       example.populateNums(name); 
+        runningTotal.textContent = `${example.getNums()} ${example.getCurrentNum()}`; 
+        
+     } else if (name == "*" || name == "/" || name == "+" || name == "-" || name == "Enter" || name == 
+     "Backspace" ) {
+        if (name == "Enter") {
+            event.preventDefault(); 
+            example.populateNums("="); 
+            display.textContent = example.operate(); 
+            return; 
+        } else if (name == "Backspace") {
+            event.preventDefault(); 
+            example.deleteCurrentNum(); 
+            runningTotal.textContent = example.getNums(); 
+            return; 
+         }
+        event.preventDefault(); 
+        example.populateNums(name); 
+        runningTotal.textContent = `${example.getNums()}`; 
+     } 
+    // alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+  }, false);
+
 let clearButton = document.querySelector("#allClear"); 
 clearButton.addEventListener("click", () => {
     example.clear(); 
+    errorText.textContent = ""; 
     runningTotal.textContent = example.getCurrentNum();
     display.textContent = 0; 
 })
@@ -29,45 +59,33 @@ let equalButton = document.querySelector("#operate");
 equalButton.addEventListener("click", (e) => {
     example.populateNums(e.target.value); 
     display.textContent = example.operate(); 
-    console.log(example.getNums()); 
 })
 
 const calc = function() {
     let nums = []; 
-    let currentNum = ""; 
+    let currentNum = []; 
     let decimalCount = 0; 
        
     return { 
         populateNums(entry){
-            //Throwing errors 
-            let lastEntry = nums[nums.length - 1]; 
-            if (lastEntry == "/" && entry == 0) {
-                errorText.textContent= "You cannot divide by 0! Try again."; 
-                throw Error("You cannot divide by 0! Try again."); 
-            } else if ((lastEntry == "/" || lastEntry == "*" || lastEntry == "+" || lastEntry == "-" ) && (entry == "/" || 
-            entry == "*" || entry == "+" || entry == "-" )) {
-                errorText.textContent = "You have to enter a number after an operator! Try again."; 
-                throw Error("You have to enter a number after an operator! Try again."); 
-            }
-
-            //Logic 
             if (entry == "/" || entry == "*" || entry == "+" || entry == "-" || entry == "=" || entry == "%") {
-                nums.push(currentNum); 
+                let formatted = example.getCurrentNum(); 
+                nums.push(formatted); 
                 nums.push(entry); 
                 decimalCount = 0; 
-                currentNum = ""; 
+                currentNum = []; 
+    
             }  else if (entry == ".") {
                 decimalCount += 1; 
                 if (decimalCount > 1) {
                     errorText.textContent = "You cannot add more than one decimal per number! Try again."; 
+                    decimalCount = 0; 
                     throw Error("You cannot add more than one decimal per number! Try again"); 
                 } 
-                    currentNum += entry; 
+                currentNum.push(entry); 
             } else {
-                currentNum += entry; 
+                currentNum.push(entry); 
             }
-        
-
         },
 
         getNums(){
@@ -75,12 +93,18 @@ const calc = function() {
         },
 
         getCurrentNum(){
-            return currentNum; 
+            return currentNum.join(""); 
         }, 
 
-        operate(){
-        
+        deleteCurrentNum(){
+            currentNum = []; 
+        }, 
 
+        editCurrentNum(val){
+            currentNum.push(val); 
+        },
+
+        operate(){
             let currentTotal = 0; 
 
             for (let i = 1; i < nums.length; i+= 2) {
@@ -100,8 +124,10 @@ const calc = function() {
                         continue; 
                     } else if (currentEl == "/") {
                         currentTotal = this.divide(prevEl, nextEl)
+                        continue; 
                     } else if (currentEl == "%") {
                         currentTotal = this.modulo(prevEl, nextEl); 
+                        continue; 
                     }
                 } 
 
@@ -118,8 +144,12 @@ const calc = function() {
                 }
 
             }
-            
-            return currentTotal; 
+
+            if (Number.isInteger(currentTotal)) {
+                return currentTotal; 
+            } else {
+                return Number(currentTotal).toFixed(15);
+            }
         },
 
         add(num1, num2){
@@ -131,24 +161,20 @@ const calc = function() {
         }, 
 
         divide(num1, num2){
-            return num1 / num2; 
+            return num1/num2; 
         }, 
 
         multiply(num1, num2) {
             return num1 * num2; 
         },
-             
-        eraseLast() {
-            nums.pop(); 
-        }, 
 
         modulo(num1, num2){
             return num1 % num2; 
         }, 
 
         clear(){
-            currentNum = ""; 
             nums = []; 
+            currentNum = []; 
         }
     }
 }
@@ -162,8 +188,9 @@ let example =  calc();
 //3. How do I create an instance of the calc for eventlistener? Can/should I use some sort of onClick func instead? 
 
 //Wiring up front end 
-//1. "Pressing = before entering all of the numbers or an operator could cause problems!"
-//2. Round decimal answers to 'long' decimals (round to 15 places?)
-//3. "Add keyboard support! You might run into an issue where keys such as (/) might cause you some trouble. Read the MDN documentation for event.preventDefault to help solve this problem."
-//4. Something about delete last not working properly-- it deletes it from display at first but then it's still there on the running tally
-    //check if the actual num it produces is still correct (i.e. the actual operator is working correctly and it's just a bug w running tally)
+//1. Not sure how to handle the logic of forcing user input to conform to [num] [operator] [num].
+    //example 1: 2 * = 
+    //example 2: 2 + 3 - 2 - = 
+    //example 3: + 2 = 
+//2. How to clear the warning sign from the UI as soon as the user fixes the problem?
+//3. Why isn't there keyboard support for "%" operator here? 
